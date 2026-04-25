a7174a4f46d93c1632e34bc59d0c5b1599e7584c
xcerock <frederick.castaneda@utp.edu.co>
2026-04-25 15:02:19 -0500

    Remove ngrok from configuration(refactor)
    Lyra runs locally on a single machine so there is no need to expose the
    server to the internet via a tunnel. This removes all ngrok-related fields
    from the config template and the env_vars namespace, simplifying the setup
    and eliminating a dead dependency path.
    
    Change log:
    - Removed ngrokPort and lyraUrl fields from templates/config.json
    - Removed ngrok_port from the env_vars SimpleNamespace in config/env_vars.py

--- config/env_vars.py
+++ config/env_vars.py
@@ -6,7 +6,6 @@ config = load_config(CONFIG_PATH)
 
 env_vars = SimpleNamespace(
     host=config["host"],
-    ngrok_port=config["ngrokPort"],
     api_port=config["apiPort"],
     mode=config["mode"],
     verbose=config["verbose"] == "1",
